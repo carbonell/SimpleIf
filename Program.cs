3cc5a298a7376206b87bdb840ede986de3b57507
Carlos Carbonell López <carbonelldx89@gmail.com>
2021-06-08 22:55:01 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ThirdExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que captura una letra y te dice si es la letra A mayúscula");
+            Console.WriteLine("Coloque una letra: ");
+            var letter = Console.ReadLine();
+            if (letter == "A")
+            {
+                Console.WriteLine("La letra es una A mayúscula!!");
+            }
+        }
+    }
+}
